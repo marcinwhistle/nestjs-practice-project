48a087b590956d2f87f194a60c4b4b57df15a82a
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-07-26 16:14:17 +0200

    Add almost ready delete func

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Delete } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-@Get('/')
+  @Get('/')
   getAll(): any {
     return this.productsService.getAll();
-}
+  }
+
+  @Get('/:id')
+  public getById(@Param('id') id: string) {
+    return this.productsService.getById(id);
+  }
+
+  @Delete('/:id')
+  public deleteById(@Param('id') id: string){
+    return
+  }
+
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,4 +6,19 @@ export class ProductsService {
   public getAll(): Product[] {
     return db.products;
   }
+
+  public getById(id: Product['id']): Product | null {
+    return db.products.find((p) => p.id === id);
+  }
+
+  public deleteById(id: Product['id']): Product | null {
+    const indexToDelete = db.products.findIndex((p) => p.id === id);
+
+    if (indexToDelete !== -1) {
+      const deletedProduct = db.products.splice(indexToDelete, 1)[0];
+      return deletedProduct;
+    } else {
+      return null;
+    }
+  }
 }
