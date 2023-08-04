f7d92663760c3d9f4c8c371be0e8f8095e7b78c9
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-08-04 15:10:41 +0200

    Edit seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,21 @@
 import { PrismaClient } from '@prisma/client';
 const db = new PrismaClient();
 
+function getClients() {
+  return [
+    {
+      id: 'fd105551-0f0d-4a9f-bc41-c559c8a17123',
+      name: 'Mac Gyver',
+      address: 'Ulica Sezamkowa 1',
+    },
+    {
+      id: 'fd105551-0f0d-4a9f-bc41-c559c8a17456',
+      name: 'John Lemon',
+      address: 'Ulica Muzyczna 123',
+    },
+  ];
+}
+
 function getProducts() {
   return [
     {
@@ -43,18 +58,21 @@ function getOrders() {
       client: 'John Doe',
       address: '123 Main Street, London',
       productId: 'fd105551-0f0d-4a9f-bc41-c559c8a17256',
+      clientId: 'fd105551-0f0d-4a9f-bc41-c559c8a17456',
     },
     {
       id: 'fd105551-0f0d-4a9f-bc41-c559c8a17261',
       client: 'Jane Doe',
       address: '123 Main Street, London',
       productId: 'fd105551-0f0d-4a9f-bc41-c559c8a17256',
+      clientId: 'fd105551-0f0d-4a9f-bc41-c559c8a17456',
     },
     {
       id: 'fd105551-0f0d-4a9f-bc41-c559c8a17262',
       client: 'Thomas Jefferson',
       address: 'Baker Street 12B, New York',
       productId: '01c7599d-318b-4b9f-baf7-51f3a936a2d4',
+      clientId: 'fd105551-0f0d-4a9f-bc41-c559c8a17123',
     },
   ];
 }
